aaae4aa6da40567115e0ef3c1b8c1a30db73b612
Samuele <samueletasciotti@icloud.com>
2025-09-30 19:09:45 +0200

    feat: New RESTFULL route for Users

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,10 @@
 import router from '@adonisjs/core/services/router'
 import DeadlinesController from '../app/controllers/DeadlinesController.js'
-
+import UsersController from '../app/controllers/UsersController.js'
 
 router.get('/', async () => {
-  return {}
+  return {'status': 'ok' }
 })
 
 router.resource('deadlines', DeadlinesController).apiOnly()
+router.resource('users', UsersController).apiOnly()
