987936a595b0773bd25f0bb871a32aa10334433b
Samuele <samueletasciotti@icloud.com>
2025-10-01 12:27:20 +0200

    feat: UsersController +  CRUD

--- app/controllers/UsersController.ts
+++ app/controllers/UsersController.ts
@@ -24,9 +24,11 @@ export default class UsersController {
         password: data.password,
         createdAt: DateTime.now(),
       })
+
+      await user.load('deadlines')
     } catch (error) {
       return response.status(400).json({
-        error: 'Impossibile creare l\'utente',
+        error: "Impossibile creare l'utente",
         message: error.message,
       })
     }
@@ -35,15 +37,54 @@ export default class UsersController {
   /**
    * Show individual record
    */
-  async show({ params, response }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    try {
+      const user = await User.query().where('id', params.id).firstOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.status(404).json({
+        error: 'Untente non trovato',
+        message: error.message,
+      })
+    }
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request, response }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const user = await User.findOrFail(params.id)
+      const data = request.only(['name', 'email', 'password'])
+
+      user.merge(data)
+      await user.save()
+
+      return response.ok(user)
+    } catch (error) {
+      return response.status(400).json({
+        error: "Impossibile aggiornare l'utente \ o non trovato",
+        message: error.message,
+      })
+    }
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params, response }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const user = await User.findOrFail(params.id)
+      await user.delete()
+
+      return response.status(200).json({
+        message: 'Utente eliminato con successo',
+      })
+    } catch (error) {
+      return response.status(400).json({
+        error: "Impossibile eliminare l'utente o non trovato",
+        message: error.message,
+      })
+    }
+  }
 }
