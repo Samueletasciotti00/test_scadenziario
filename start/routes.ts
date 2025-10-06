558abe5f57f787bf7eef8c9d9ab0610e63b558fe
Samuele <samueletasciotti@icloud.com>
2025-10-06 10:24:53 +0200

    feat: new routes for login/logout/register

--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,28 @@
 import router from '@adonisjs/core/services/router'
 import DeadlinesController from '../app/controllers/DeadlinesController.js'
 import UsersController from '../app/controllers/UsersController.js'
+import AuthController from '../app/controllers/auth_controller.js'
+
 
 router.get('/', async () => {
   return {'status': 'ok' }
 })
 
+// Rotta per registrazione
+router.post('/register', async (ctx) => {
+  return new AuthController().register(ctx)
+})
+
+// Rotta per login
+router.post('/login', async (ctx) => {
+  return new AuthController().login(ctx)
+})
+// Rotta per logout
+router.post('/logout', async (ctx) => {
+  return new AuthController().logout(ctx)
+})
+
+
 router.resource('deadlines', DeadlinesController).apiOnly()
 router.resource('users', UsersController).apiOnly()
+
