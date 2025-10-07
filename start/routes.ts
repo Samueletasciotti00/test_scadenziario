5ec00af8283190c913882edfdc457fd496f9ac91
Samuele <samueletasciotti@icloud.com>
2025-10-07 16:22:43 +0200

    fix: add router group for deadlines actions.
    feat: Middleware routes

--- start/routes.ts
+++ start/routes.ts
@@ -3,25 +3,38 @@ import DeadlinesController from '../app/controllers/DeadlinesController.js'
 import UsersController from '../app/controllers/UsersController.js'
 import AuthController from '../app/controllers/auth_controller.js'
 
+// Import middleware
+import { middleware } from './kernel.js'
 
 // Rotta di test
-router.get('/', async () => { return {'status': 'ok' } })
+router.get('/', async () => {
+  return { status: 'ok' }
+})
 
-// Rotte CRUD per deadlines e users (Manuale)
-router.resource('deadlines', DeadlinesController).apiOnly()
+// Rotte aggiuntive per utenti loggati
+router
+  .group(() => {
+    router.get('/deadlines', [DeadlinesController, 'index'])
+    router.post('/deadlines', [DeadlinesController, 'store'])
+    router.put('/deadlines/:id', [DeadlinesController, 'update'])
+    router.delete('/deadlines/:id', [DeadlinesController, 'destroy'])
+  })
+  .use([middleware.auth(), middleware.isAdmin()])
+  
+router.post('/deadlines/:id/take', [DeadlinesController, 'taken']).use([middleware.auth()])
+// Rotta per aggiornare solo lo stato della deadline da utente loggato
+router.patch('/deadlines/:id/status', [DeadlinesController, 'updateStatus']).use([middleware.auth(), middleware.checkDeadlineOwnership()])
+
+// Rotte CRUD per users (Manuale)
 router.resource('users', UsersController).apiOnly()
 
-// Rotta per registrazione
+// Auth routes
 router.post('/register', async (ctx) => {
   return new AuthController().register(ctx)
 })
-
-// Rotta per login  
 router.post('/login', async (ctx) => {
   return new AuthController().login(ctx)
 })
-
-// Rotta per logout
 router.post('/logout', async (ctx) => {
   return new AuthController().logout(ctx)
 })
